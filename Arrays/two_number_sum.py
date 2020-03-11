287a056916c2bffcc74eb852ab0c55ee814f1314
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-03-11 16:50:40 -0700

    added two number solution

---
+++ Arrays/two_number_sum.py
@@ -0,0 +1,17 @@
+# Write a function that takes in a non-empty array of distinct integers and an integer representing a target sum. If any two numbers in the input array sum up to the target sum, the function should return them in an array. If no two numbers sum up to the target sum, the function should return an empty array. Assume that there will be at most one pair of numbers summing up to the target sum.
+
+def twoNumberSum(array, targetSum):
+    # brute force
+    # length = len(array)
+    # for idx in range(length):
+    # 	for idx2 in range(idx+1, length):
+    # 		if array[idx] + array[idx2] == targetSum:
+    # 			return [array[idx], array[idx2]]
+
+    # improved. Keep a dictionary to store each number you're looking for and if you find it, just return the pair
+    passed = {}
+    for item in array:
+        if targetSum - item in passed:
+            return [item, targetSum - item]
+    passed[item] = True
+    return []
