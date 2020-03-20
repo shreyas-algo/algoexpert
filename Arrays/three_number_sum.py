6cbb5dc8216b2af3c4e0006556921c17b467ad2a
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-03-19 18:59:41 -0700

    Added approach

--- Arrays/three_number_sum.py
+++ Arrays/three_number_sum.py
@@ -1,3 +1,6 @@
+# Approach used: Break dowmn in a problem of two-sum. For every number, create a 2_sum_target (called target in code) which you look for in the remaining array. The question basically becomes n iterations of two-sum
+# Sort & sorted() used to satisfy the given condition of result array being sorted
+
 def threeNumberSum(array, targetSum):
     result = []
 	length = len(array)
