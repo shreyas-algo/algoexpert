72114f5b77f52ccd25eccfc5b6e7c6479ecbd361
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-03-21 00:42:52 -0700

    Added approach to move to end

---
+++ Arrays/move_to_end.py
@@ -0,0 +1,8 @@
+# Approach I: Create a copy array of same size (initialized with 0). Iterate through the given array and start putting the target element at back of the array one by one and start putting other elements from start
+# Complexity: O(N) time & O(N) space
+
+# Approach II: Sort so that all target element is together and then swap elements that come after the target element 
+# Complexity: O(N log N) time & O(1) space
+
+# Approach III: Traverse thorugh the array and keep swapping other numbers with last non-target element you saw
+# Complexity: O(N) time & O(1) space
