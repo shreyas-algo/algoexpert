441590dc01f12e20ca039d5c2818da636bc4fe70
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-04-25 13:21:38 -0700

    Added basic code structure using binary search

--- Searching/Shifted_Bin_Search.py
+++ Searching/Shifted_Bin_Search.py
@@ -1,25 +1,24 @@
 def shiftedBinarySearch(array, target):
-  start_index = findShiftIndex(array)
-  result = shiftedBinarySearchHelper(array, target, start_index)
-  return result
-
-def shiftedBinarySearchHelper(a, target, start_index):
-  arr_len = len(array)
-  if arr_len == 0:
-    return -1
-  mid = (start_index + int(arr_len/2))%arr_len
-  if a[mid] == target:
-    return mid
-  elif a[mid] > target:
-    if start_index < mid:
-      shiftedBinarySearchHelper(a[start_index:mid], target, start_index)
-    else:
-      shiftedBinarySearchHelper(a[start_index:]+a[:mid], target, start_index)
-  else:
-    if end_indx > mid:
-      shiftedBinarySearchHelper(a[mid+1:end_indx], target, start_index)
-    else:
-      shiftedBinarySearchHelper(a[mid+1:]+a[:end_indx], target, start_index)
+    result = -1
+    start = findShiftIndex(array)
+    arr_len = len(array)
+    curr_len = arr_len
+    end = arr_len - 1
+    if start == 0:
+        end = start - 1
+    # keep searching unless there are elements in the search space
+    while curr_len > 0:
+        # notice through an example
+        mid = (start + int(len/2))%curr_len
+        print("checking {}: {}".format(mid, array[mid]))
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:   # check left
+            end = mid - 1
+        else:
+            start = mid + 1         # check right
+        # todo: add code similar to bin search. Just indexed out of start_index. Understand that you don't need to have the whole array everytime. All you need is: mid, length, start_index (Using these you can always )
+    return result
 
 def findShiftIndex(array):
   # TODO: create logic to find shift point
