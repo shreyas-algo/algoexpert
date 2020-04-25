8aa98f123074c31c0b350cf3c6a0d895e3e01dd7
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-04-25 14:37:36 -0700

    1st  case working end to end

--- Searching/Shifted_Bin_Search.py
+++ Searching/Shifted_Bin_Search.py
@@ -4,24 +4,33 @@ def shiftedBinarySearch(array, target):
     arr_len = len(array)
     curr_len = arr_len
     end = arr_len - 1
+    count = 0
     if start == 0:
         end = start - 1
     # keep searching unless there are elements in the search space
-    while curr_len > 0:
-        # notice through an example
-        mid = (start + int(len/2))%curr_len
-        print("checking {}: {}".format(mid, array[mid]))
+    while curr_len > 0 and count != 10:
+        # notice through an example -- mid indexing is always based on start
+        mid = (start + int(curr_len/2))%arr_len
+        # print("Current length: {}".format(curr_len))
+        # print("checking {}: {}".format(mid, array[mid]))
         if array[mid] == target:
             return mid
         elif array[mid] > target:   # check left
             end = mid - 1
         else:
             start = mid + 1         # check right
-        # todo: add code similar to bin search. Just indexed out of start_index. Understand that you don't need to have the whole array everytime. All you need is: mid, length, start_index (Using these you can always )
+            # print("Right. & new start: {}".format(start))
+        
+        if start <= end:
+            curr_len = end - start + 1
+        else:
+            curr_len = ((end + arr_len) - start) + 1
+        # print("?> Count: {}".format(count))
+        count += 1
     return result
 
 def findShiftIndex(array):
   # TODO: create logic to find shift point
   return 5
 
-shiftedBinarySearch([45, 61, 71, 72, 73, 0, 1, 21, 33, 45])
+print(shiftedBinarySearch([45, 61, 71, 72, 73, 0, 1, 21, 33, 45], 33))
