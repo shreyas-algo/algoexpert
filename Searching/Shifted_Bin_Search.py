6c14c843935d4e06e4e72509f850957c8debea8e
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-04-25 22:53:15 -0700

    Issue: [5, 23, 111, 1] because origin / start was shifted to an index which breaks the sorted nature

--- Searching/Shifted_Bin_Search.py
+++ Searching/Shifted_Bin_Search.py
@@ -17,8 +17,9 @@ def shiftedBinarySearch(array, target):
     while curr_len > 0:
         # notice through an example -- mid indexing is always based on start
         mid = (start + int(curr_len/2))%arr_len
-        # print("Current length: {}".format(curr_len))
-        # print("checking {}: {}".format(mid, array[mid]))
+        print("Current length: {}".format(curr_len))
+        print("checking {}: {}".format(mid, array[mid]))
+        print("*********")
         if array[mid] == target:
             return mid
         elif array[mid] > target:   # check left
@@ -27,18 +28,12 @@ def shiftedBinarySearch(array, target):
         else:
             start = mid + 1         # check right
             # print("Right. & new start: {}".format(start))
-        
-        # if start <= end:
-        #     curr_len = end - start + 1
-        # else:
-        #     curr_len = ((end + arr_len) - start) + 1
-        # print("?> Count: {}".format(count))
         curr_len = int(curr_len/2)
         count += 1
     return result
 
 # find index where origin has shifted to. it'll basically be a dip
-# todo: add logic to handle equality
+# todo: add logic to handle equality?
 def findShiftIndex(array):
   array_len = len(array)
   if array_len == 0: return 0
@@ -50,11 +45,13 @@ def findShiftIndex(array):
     current = array[idx]
   return 0
 
-print(shiftedBinarySearch([45, 61, 71, 72, 73, 0, 1, 21, 33, 45], 33))  # 8
-print(shiftedBinarySearch([33, 45, 45, 61, 71, 72, 73, 0, 1, 21], 33))  # 0
-print(shiftedBinarySearch([33, 45, 45, 61, 71, 72, 73, 0, 1, 21], 0))  # 7
-print(shiftedBinarySearch([45, 61, 71, 72, 73, 0, 1, 21, 33, 45], 61))  # 1 
-print(shiftedBinarySearch([45, 61, 71, 72, 73, 0, 1, 21, 33, 45], 610))  # -1 
+# print(shiftedBinarySearch([45, 61, 71, 72, 73, 0, 1, 21, 33, 45], 33))  # 8
+# print(shiftedBinarySearch([33, 45, 45, 61, 71, 72, 73, 0, 1, 21], 33))  # 0
+# print(shiftedBinarySearch([33, 45, 45, 61, 71, 72, 73, 0, 1, 21], 0))  # 7
+# print(shiftedBinarySearch([45, 61, 71, 72, 73, 0, 1, 21, 33, 45], 61))  # 1 
+# print(shiftedBinarySearch([45, 61, 71, 72, 73, 0, 1, 21, 33, 45], 610))  # -1 
+# print(shiftedBinarySearch([0, 1, 21, 33, 45, 65, 99], 99))  # 6
+print(shiftedBinarySearch([5, 23, 111, 1], 111))  # 2 -> my ans: -1 -- because shift of origin causes bin search logic to break
 
 
 # with all comments and logic
