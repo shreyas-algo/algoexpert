81cb0291967e9861d268add9ae5ffc71b65839a0
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-05-02 12:44:53 -0700

    Added print statement

--- Binary Tree/branch_sums.py
+++ Binary Tree/branch_sums.py
@@ -6,10 +6,12 @@ class BinaryTree:
         self.right = None
 
 # Status - Does not run succcesfully when "Run Tests" used on Algoexpert. But works when single test case is populated and checked
+# Update: Added print and compared to test cases. It looks good. Possibly the issue is how Algoexpert is calling the function
 # curr_sum = 0
 # result = []
 def branchSums(root, curr_sum=0, result=[]):
     if root.left == None and root.right == None:
+        print("leaf: {}, sum: {}".format(root.value, curr_sum + root.value))
         result.append(curr_sum + root.value)
     if root.left != None:
         branchSums(root.left, curr_sum + root.value, result)
