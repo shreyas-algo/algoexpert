8e2effcc2fa11e3b3f2e5c7fd7e6b11de4a894c0
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-05-03 12:15:27 -0700

    Added optimization -- cleaned code

--- Graphs/single_cycle_check.py
+++ Graphs/single_cycle_check.py
@@ -1,22 +1,30 @@
 # Brute Force: for every index, go over the hops and see if items repeat
 # O(N*N) time - for every item, you'll traverse the whole array in case if it's a single cycle array (True case)
+
+# Optimization: Think about it. You dont actually need to check all indexes. if you start from 0 and reach 0 without any repetition - that means the path is a single cycle which does not repeat any index. Which means no matter where you start you will have a successful single cycle
+# O(N) time & O(1) space
+
 def hasSingleCycle(array):
     arr_len = len(array)
-    for idx, val in enumerate(array):
-        curr_idx = idx
-        # curr_val = array[curr_idx] 
-        passed = []
-        count = 0
-        while True:
-            if curr_idx == idx and count == arr_len:
-                return True
-            if curr_idx in passed:
-                print(idx, curr_idx)
-                return False	
-            else:
-                passed.append(curr_idx)
-                curr_idx = (curr_idx + array[curr_idx]) % arr_len
-            count += 1
+    curr_idx = 0
+    passed = []
+    count = 0
+    while True:
+        # if you're back to 0 and you traversed whole array elements (count check) -- then it's a single cycle input
+        if curr_idx == 0 and count == arr_len:
+            return True
+        # if you have already traversed this index without completing the single cycle path, there is a copy visit, hence False
+        if curr_idx in passed:
+            return False	
+        else:
+        # visit next, if all good
+            passed.append(curr_idx)
+            curr_idx = (curr_idx + array[curr_idx]) % arr_len
+        count += 1
     return True
 
-# Optimization: ?
+hasSingleCycle([2, 3, 1, -4, -4, 2])
+# single cycle 
+# complete index(value_at_index):
+# 0(2) -> 2(1) -> 3(-4) -> -1(2) -> 1(3) -> 4(-4) -> 0 :: True
+
