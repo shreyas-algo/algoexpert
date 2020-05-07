a2c5f0a29ae49ec2e1be12cb8fdb233b6f72b159
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-05-06 21:37:44 -0700

    Added is palindrome

---
+++ Strings/isPalindrome.py
@@ -0,0 +1,9 @@
+def isPalindrome(string):
+    lo = 0
+    hi = len(string) - 1
+    while lo <= hi:
+        if string[lo] != string[hi]:
+            return False
+        lo += 1
+        hi -= 1
+    return True
