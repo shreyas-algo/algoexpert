fe5d092a7f7d41141fc405c3a6458e7079f30d8d
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-05-06 22:19:46 -0700

    Added caesar cipher

---
+++ Strings/caesar_cipher.py
@@ -0,0 +1,11 @@
+def caesarCipherEncryptor(string, key):
+    result = []
+    base = ord('a')
+    for ch in string:
+        # to wrap around 26 alphabets and to start from a -> 0. ord returns ascii which is 97 for a
+        shift = ((ord(ch) - base) + key) % 26
+        # change back to character shifted back to ascii
+        char = chr(shift + base)
+        result.append(char)
+    # array & ''.join used cz str += takes O(n) time because of how strings are implemented (read more)
+    return ''.join(result)
