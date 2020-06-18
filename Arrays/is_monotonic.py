e17af98af8da1fde74d1cf6ea4cb6bf5151d0223
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-06-17 18:31:11 -0700

    Improved wording

--- Arrays/is_monotonic.py
+++ Arrays/is_monotonic.py
@@ -1,13 +1,15 @@
-# check whether an array is strictly incresaing or strictly decreasing
+# check whether an array is non-incresaing or  non-decreasing
+# non-incresaing: strictly increasing or some values equal
+
 # Approach: Get order pattern by checking first two unequal numbers. Then iterate the array again checking that all elements follow the same pattern (increasing or decreasing)
 # Analysis: O(2N) = O(N)
 def isMonotonic(array):
     arr_len = len(array)
     order = checkOrder(array, arr_len)
     for idx in range(arr_len-1):
-        if order == "inc" and array[idx] < array[idx+1]:
+        if order == "non-dec" and array[idx] < array[idx+1]:
             return False
-        if order == "dec" and array[idx] > array[idx+1]:
+        if order == "non-inc" and array[idx] > array[idx+1]:
             return False
     return True
 
@@ -18,9 +20,9 @@ def checkOrder(array, arr_len):
 			# equality doesn't give decisive info
 			continue
 		elif array[idx] > array[idx+1]:
-			return "inc"
+			return "non-dec"
 		else:
-			return "dec"
+			return "non-inc"
 	# all values are same in array
-	return "inc"
+	return "non-dec"
 
