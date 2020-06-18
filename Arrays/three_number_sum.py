d511b90877daf1d6f01baa37085e012d7981526e
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-06-17 20:19:42 -0700

    corrected analyisis

--- Arrays/three_number_sum.py
+++ Arrays/three_number_sum.py
@@ -4,20 +4,20 @@
 # Space complexity: O(N*N) -- passed set of ~N length stored for every number
 def threeNumberSum(array, targetSum):
     result = []
-	length = len(array)
-	array.sort()
-	for idx in range(length-1):
-		target = targetSum - array[idx]
-		passed = set()
-		for idx2 in range(idx+1, length):
-			if target - array[idx2] in passed:
-				result.append([array[idx], target - array[idx2], array[idx2]])
-			passed.add(array[idx2])
+    length = len(array)
+    array.sort()
+    for idx in range(length-1):
+        target = targetSum - array[idx]
+        passed = set()
+        for idx2 in range(idx+1, length):
+            if target - array[idx2] in passed:
+                result.append([array[idx], target - array[idx2], array[idx2]])
+            passed.add(array[idx2])
     return sorted(result)
 
 # II. Approach used: Sort & Sliding Window / 2 pointer
 # Time complexity: O(N*N)
-# Space complexity: O(N)
+# Space complexity: O(1)
 # Algoexpert solution. No extra space. Sliding window
 def threeNumberSum(array, targetSum):
     result = []
