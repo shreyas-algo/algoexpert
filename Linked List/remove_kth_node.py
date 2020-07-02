e7a6c7db0f15820353e045c2d52a2aea85479e23
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-07-02 00:21:37 -0700

    Added working solution and comments

--- Linked List/remove_kth_node.py
+++ Linked List/remove_kth_node.py
@@ -1,7 +1,11 @@
 # Approach: Get length of linked list. Get node to remove and its previous (so that next can be updated). Remove the node by updating bindings
 # Analysis: O(N) time, O(1) space
 
-# Status: Just one case not working when head needs to be updated
+# Status: All working. Pointers of head updated. But strange to see how left side of assignment also affected. as I changed the head on line 24: `head.next = nodeToRemove.next.next`, nodeToRemove is also changed. Hence when later we change `nodeToRemove.next = None` to change old head's next pointer, the upfated head loses next (changes to None). Basically head pointer and nodeToRemove pointers are tightly (two-way) coupled. Solution is to return in case of head removal
+
+# Note: 
+# 1. All comments and detailing is with respect to a single case: head removal
+# 2. Ignore all comments and print statements if you just want to focus on the problem. Read comments after. The solution itself is pretty simple
 
 # This is an input class. Do not edit.
 class LinkedList:
@@ -16,9 +20,15 @@ def removeKthNodeFromEnd(head, k):
 	# removing head
 	if head == nodeToRemove:
 		# Issue: Updating local parameter here doesn't change the actual LinkedList's head (variable scoping. local var changes are not reflected outside functions). For that you need reference to the LinkedList. Though this technique to update head is great and logically makes sense. There doesn't seem to be a way to update the linked list head unless a reference is passed.
-		head = nodeToRemove.next
+		# head = nodeToRemove.next
+		print(nodeToRemove.next.value)  # 1
+		head.value = nodeToRemove.next.value
+		head.next = nodeToRemove.next.next
+		print(nodeToRemove.next.value)  # 2 What? nodeToRemove.next changed as we changed head.next! Interesting
+		return                          # Due to the issue seen in print statements, this return is important. It shouldn't be. Some pointer / Algoexpert issue
 	else:
 		prev.next = nodeToRemove.next
+	print(nodeToRemove.next.value)      # Prints 2 instead of 1 if return not used for the head removal case. Strange!
 	nodeToRemove.next = None
     return
 
