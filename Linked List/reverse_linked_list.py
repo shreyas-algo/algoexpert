acafc39412b032b4b4f2b00f65ac751c26b6e73b
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-07-11 14:41:33 -0700

    comment changes

--- Linked List/reverse_linked_list.py
+++ Linked List/reverse_linked_list.py
@@ -1,4 +1,4 @@
-# Approach: Simple approach to keep the next pointer safe in a temp variable (link) so that you can access it to keep traversing and chenge current node's "next" at the same time so that the linked list can be reversed
+# Approach: Simple approach to keep the next pointer safe (from overriding) in a temp variable (link) so that you can access it to keep traversing and chenge current node's "next" at the same time so that the linked list can be reversed
 # Analysis: O(N)
 def reverseLinkedList(head):
     curr = head
