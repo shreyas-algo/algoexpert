c7b91ae81d2b32901c7cc2734197f1c7646de43c
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-07-16 09:31:09 -0700

    Attempt II at productSum

--- Recursion/productSum.py
+++ Recursion/productSum.py
@@ -2,16 +2,29 @@
 # is a list or an integer.
 # mul = 1
 # sum = 0
-def productSum(array):
-	return findSum(array, 0, 1)
+# def productSum(array):
+# 	return findSum(array, 0, 1)
 
 		
-def findSum(array, sum, mul):
-	if array:
-		item = array.pop(0)
-		if type(item) == int:
-			sum += item
-			productSum(array)
-		mul += 1
-		return sum + findSum(item, sum, mul)
-	return sum 
+# def findSum(array, sum, mul):
+# 	if array:
+# 		item = array.pop(0)
+# 		if type(item) == int:
+# 			sum += item
+# 			productSum(array)
+# 		mul += 1
+# 		return sum + findSum(item, sum, mul)
+# 	return sum 
+
+
+def productSum(array, sum=0, mul=1):
+    if not array or type(array) == int:
+		return sum
+	# print(array)
+	item = array.pop(0)
+	if type(item) == int:
+		print(item, mul, item * mul, sum)
+		sum += item * mul
+		return productSum(array, sum, mul)
+	sum = productSum(item, sum, mul+1)
+	return productSum(array, sum, mul)
