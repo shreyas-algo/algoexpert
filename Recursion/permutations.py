4fb649924922c33b793b4dffe9a18ba81b132bd0
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-07-30 10:36:16 -0700

    Added memoization but still has memory issues

--- Recursion/permutations.py
+++ Recursion/permutations.py
@@ -1,15 +1,28 @@
 # Approach: Iterate through the array and for every element, append the element in the permutations of {array - element}. Thus `perm(contains: n) = perm(without n) + n` thus it is recursive definition with base case being only 2 elements when the result is {a, b} & {b ,a}
-# Optimize: Memoize results
+# Optimize: Memoize results: use tuple(list)
+
+# Note: frozenset can be used because an array of unique integers given. (or else frozenset() will obliterate non-unique numbers)
+# Try thinking how you'll solve it of it was non-unique integers
+# IMP: Notice that frozenset used for two things: a) to create key of dictionary (tuple can be used instead) b) to obtain set difference (will need new logic)
+# Talk with your interviewer. Try to get a solution ready as below (consider only unoque numbers) which will fail some cases 
 
 result = []
+dict = {}
 def getPermutations(array):
 	if len(array) == 2:
 		return [[array[0], array[1]], [array[1], array[0]]]
     for integer in array:
         # get all permutations except current integer
-		remaining = getPermutations(list(set(array) - set([integer])))
+        remainingSet = frozenset(set(array) - set([integer]))
+        if remainingSet in dict:
+			print("yes")
+            remaining = dict[remainingSet]
+		else:
+            remaining = getPermutations(list(remainingSet))
+            dict[remainingSet] = remaining
         # append current integer in all results
 		for rem in remaining:
 			rem.append(integer)
 			result.append(rem)
     return result
+
