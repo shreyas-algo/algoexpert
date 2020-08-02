5bd4b186ee3e016dc6d9b7d8319fd5adda8b2fb5
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-08-01 22:28:06 -0700

    Added init

--- Recursion/permutations.py
+++ Recursion/permutations.py
@@ -6,23 +6,29 @@
 # IMP: Notice that frozenset used for two things: a) to create key of dictionary (tuple can be used instead) b) to obtain set difference (will need new logic)
 # Talk with your interviewer. Try to get a solution ready as below (consider only unoque numbers) which will fail some cases 
 
-result = []
-dict = {}
-def getPermutations(array):
+def getPermutations(array, count = 0, dict = {}, result = []):
+	if count == 10:
+		return result
+	count += 1
 	if len(array) == 2:
 		return [[array[0], array[1]], [array[1], array[0]]]
     for integer in array:
         # get all permutations except current integer
         remainingSet = frozenset(set(array) - set([integer]))
         if remainingSet in dict:
-			print("yes")
-            remaining = dict[remainingSet]
-		else:
-            remaining = getPermutations(list(remainingSet))
-            dict[remainingSet] = remaining
-        # append current integer in all results
-		for rem in remaining:
-			rem.append(integer)
-			result.append(rem)
+        print("yes")
+                remaining = dict[remainingSet]
+        else:
+                remaining = getPermutations(list(remainingSet), count, dict, result)
+                dict[remainingSet] = remaining
+            # append current integer in all results
+        print(integer, remaining)
+        for rem in remaining:
+        rem.append(integer)
+        result.append(rem)
+        # print(integer, rem)
+        # return remaining
     return result
+
+getPermutations([1,2,3])
 
