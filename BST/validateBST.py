bbf5eefa1c08419d31ffb061ad1e4567409c9163
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-08-16 15:56:12 -0700

    Added approach

--- BST/validateBST.py
+++ BST/validateBST.py
@@ -5,13 +5,14 @@ class BST:
         self.left = None
         self.right = None
 
-
+# Approach: check if the current structure is right and then left and right children are also validBSTs. If yes, overall it's a validBST
 def validateBst(tree):
     if tree is None:
 		return True
 	if validBSTStructure(tree.left, tree, tree.right) and validateBst(tree.left) and validateBst(tree.right):
 		return True
 	else:
+		print("returning false on: ", tree.value)
 		return False
 
 def validBSTStructure(left, root, right):
