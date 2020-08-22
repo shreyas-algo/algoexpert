b4c3a3e4d1bf115825c9d97f67cc42007ade5698
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-08-22 13:14:16 -0700

    added node_depths question

---
+++ Binary Tree/node_depths.py
@@ -0,0 +1,23 @@
+# Approach: Keep calling the function with new depth on every level and keep track of sumDepth
+# O(n) time and O(d) space where d is max depth
+
+# TODO: Look at Algoexpert video
+# Learning: 
+# When an array is involved, you don't need to propogate child results eg see branch_sums.py
+# Whereas when it's a variable you need to because whatever happened in child calls, the parent is unaware of it
+def nodeDepths(root, depth=0, sumDepth=0):
+    if root is None:
+		return sumDepth
+	sumDepth += depth
+	sumDepth = nodeDepths(root.left, depth+1, sumDepth)
+	sumDepth = nodeDepths(root.right, depth+1, sumDepth)
+    # finally
+	return sumDepth
+
+# -------------- Provided base class
+# This is the class of the input binary tree.
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
