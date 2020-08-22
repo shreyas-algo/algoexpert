eda47549f43c414732b792ae842e950cd0dbce36
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-08-22 10:17:03 -0700

    Updated branch sum

--- Binary Tree/branch_sums.py
+++ Binary Tree/branch_sums.py
@@ -8,14 +8,14 @@ class BinaryTree:
 # Approach: calculated sum in DFS fashion. Appened array on leaf nodes. Passed curr_sum as parameter
 # Status - Does not run succcesfully when "Run Tests" used on Algoexpert. But works when single test case is populated and checked
 # Update: Added print and compared to test cases. It looks good. Possibly the issue is how Algoexpert is calling the function
-# curr_sum = 0
-# result = []
-def branchSums(root, curr_sum=0, result=[]):
-    if root.left == None and root.right == None:
-        print("leaf: {}, sum: {}".format(root.value, curr_sum + root.value))
-        result.append(curr_sum + root.value)
-    if root.left != None:
-        branchSums(root.left, curr_sum + root.value, result)
-    if root.right != None:
-        branchSums(root.right, curr_sum + root.value, result)
-    return result
+
+def branchSums(root, result=[], curr_sum=0):
+	if root is None:
+		return
+	curr_sum = curr_sum + root.value
+	if root.left is None and root.right is None:
+		result.append(curr_sum)
+		return
+	branchSums(root.left, result, curr_sum)
+	branchSums(root.right, result, curr_sum)
+	return result
