b38f1bdf93cf92d7ba4944e88ba24c59f5cd15b8
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-09-03 11:00:22 -0700

    Added comments and few corrections

--- Tries/suffix_trie_construction.py
+++ Tries/suffix_trie_construction.py
@@ -2,6 +2,9 @@
 # populateSuffixTrieFrom and contains methods.
 # Feel free to add new properties and methods
 # to the class.
+
+# Approach: Look at conceptual overview. Simply followed definition of Suffix Trie
+
 class SuffixTrie:
     def __init__(self, string):
         self.root = {}
@@ -11,11 +14,14 @@ class SuffixTrie:
     def populateSuffixTrieFrom(self, string):
         length = len(string)
 		for i in range(length):
+			# get back to root for every suffix start
 			current_dict = self.root
 			for j in range(i,length):
-				if j not in current_dict:
-					current_dict[j] = {}
-				current_dict = current_dict[j]
+				if string[j] not in current_dict:
+					# if dictionary for a character doesn't exist, create it
+					current_dict[string[j]] = {}
+				current_dict = current_dict[string[j]]
+			# add * at the end of a suffix
 			current_dict[self.endSymbol] = True
 
     def contains(self, string):
