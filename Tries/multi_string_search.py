11c69458804f0f535f1b9a224c4050daf5d8dd5e
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-09-04 02:23:02 -0700

    Added multi string search - working end to end

--- Tries/multi_string_search.py
+++ Tries/multi_string_search.py
@@ -8,42 +8,41 @@
 # 2. Discuss the meaning of "contains" with your interviewer - does it mean exact word or substring is okay. what about spaces - eg is " am " contained in "I am passionate!". How about "I a"?
 
 def multiStringSearch(bigString, smallStrings):
-    word_trie = WordTrie(bigString)
-    print(word_trie.root)
-    result = []
-    for string in smallStrings:
-      result.append(word_trie.contains(string))
-    return result
+  word_trie = SuffixTrie(bigString)
+  print(word_trie.root)
+  result = []
+  for string in smallStrings:
+    result.append(word_trie.contains(string))
+  return result
       
 
-class WordTrie:
-    def __init__(self, string):
-        self.root = {}
-        self.endSymbol = "*"
-        self.populateWordTrieFrom(string)
+class SuffixTrie:
+  def __init__(self, string):
+    self.root = {}
+    self.endSymbol = "*"
+    for word in string.split(" "):
+      self.populateSuffixTrieFrom(word)
 
-    def populateWordTrieFrom(self, string):
-        n = len(s)
-        result = []
-        for i in range(n): 
-          temp="" 
-          current_dict = self.root
-          for j in range(i,n): 
-              temp+=s[j]
-              if s[j] not in current_dict:
-                # if dictionary for a character doesn't exist, create it
-                current_dict[s[j]] = {}
-          current_dict = current_dict[s[j]]
-          # add * at the end of a suffix
-          current_dict[self.endSymbol] = True
+  def populateSuffixTrieFrom(self, string):
+    length = len(string)
+    for i in range(length):
+      # get back to root for every suffix start
+      current_dict = self.root
+      for j in range(i,length):
+        if string[j] not in current_dict:
+          # if dictionary for a character doesn't exist, create it
+          current_dict[string[j]] = {}
+        current_dict = current_dict[string[j]]
+      # add * at the end of a suffix
+      current_dict[self.endSymbol] = True
 
-    def contains(self, string):
-        current_dict = self.root
-        for char in string:
-            if char not in current_dict:
-                return False
-            current_dict = current_dict[char]
-        # Correct cz half words also considered right
-        # if self.endSymbol in current_dict:
-        #     return True
-        return True
+  def contains(self, string):
+    current_dict = self.root
+    for char in string:
+        if char not in current_dict:
+          return False
+        current_dict = current_dict[char]
+    # Correct cz half words also considered right
+    if self.endSymbol in current_dict:
+      return True
+    return True
