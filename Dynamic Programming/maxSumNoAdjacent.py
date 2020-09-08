9ec1d11eb96ee3af560ee6c3dc6349e3254eebe6
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-09-08 13:01:07 -0700

    Corrected typo

--- Dynamic Programming/maxSumNoAdjacent.py
+++ Dynamic Programming/maxSumNoAdjacent.py
@@ -12,7 +12,7 @@ def maxSubsetSumNoAdjacent(array):
 	result.append(array[0])
 	result.append(max(array[0], array[1]))
     for idx,item in enumerate(array):
-		next_item = max(item+array[idx-2], array[idx-1])
+		next_item = max(item+result[idx-2], result[idx-1])
 		result.append(next_item)
 		
 	return result.pop()
