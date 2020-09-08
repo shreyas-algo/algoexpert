df9b89323c9348f3deb1b475b8a5dc5b6d3644fe
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-09-08 16:16:31 -0700

    Added approach for max sum with no alternate

--- Dynamic Programming/maxSumNoAdjacent.py
+++ Dynamic Programming/maxSumNoAdjacent.py
@@ -1,3 +1,20 @@
+# Approach: 
+# Build a result array which stores result at every index (max sum with no alternates until index i) 
+# Process:
+# Compare array[i]+result[i-2] with result[i-1] and set the larger value as result[i]. Initialize result array with [array[0], array[1]]
+
+# Logic: if every index' value is a valid result, then current item (array[i]) can only be included in result[i] with result[i-2] cz result[i-1] may have array[i-1] included and we need to avoid adjacent elements. In case array[i-1] is not included in result[i-1], result[i-1] will be same as result[i-2] (max logic) so result[i-1] is covered in final value of result[i]
+# For example:
+# array:    [100   1       99      100]
+# result:   [100   100     199     200]
+
+# Analysis: O(n) time and O(n) space
+
+# IMP Learning:
+# 1. HINTS! They are there for the taking. Ask for help and hints if you can't breach a problem. 
+# It's much BETTER than bombing the inteview and not making any progress
+# Background: Was easily able to solve this question when read HINT #1: Try to create a solution array where at each index you store the maximum possible sum (by using alternate elements) until that index
+
 def maxSubsetSumNoAdjacent(array):
 	# aedge cases
 	arr_len = len(array)
