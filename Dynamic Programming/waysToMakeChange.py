2fbf9b4fcce657c8f54d36283b20a2288694105b
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-09-12 16:08:34 -0700

    partial solution working

--- Dynamic Programming/waysToMakeChange.py
+++ Dynamic Programming/waysToMakeChange.py
@@ -1,25 +1,43 @@
-def numberOfWaysToMakeChange(n, denoms, min_denom, results={}, count=0):
-	# if results not initialized (parent call), initialize it with demons: set result for each denom as 1 so that we ca return in O(1) time when the target(n) == one of t
-	# also set the min_denom
-    if not results:
-		if len(denoms) > 0:
-			min_denom = denoms[0]
-		else:
-			return 0
-		for denom in denoms:
-			if denom < min_denom:
-				min_denom = denom
-			results[denom] = 1
-	
-	# base cases
+def numberOfWaysToMakeChange(n, denoms):
+	# edge case
+	if len(denoms) == 0:
+		return 0
+	# ways stores number of ways to store a value i
+	ways = []
+	# calculated keeps track of pairs for which ways have already been added
+	# just declared here for saving space. re init in findWays function
+	calculated = set()
+	# initialize ways & find minimum value in denoms as it can be useful in skipping a few operations
+	ways, min_denom = initialize(ways, denoms)
+	for i in range(1, n+1):
+		res = findWays(i, denoms, ways, calculated, min_denom)
+		print(res)
+		ways.append(res)
+	return ways.pop()
+
+# def initialize(ways, denoms):
+# 	# number of ways to make a 0 is 1. Use no coin
+# 	ways.append(1)
+# 	min_denoms = denoms[1]
+# 	for denom in denoms:
+# 		ways
+
+def findWays(n, denoms, ways, calculated, min_denom):
+	current_ways = 0
+	print(n, denoms, ways)
+	# min_denom improves average case. Doesn't help the worst case
 	if n < min_denom:
 		return 0
-	if n in results:
-		return results[n]
-	
 	for denom in denoms:
-		while n  >= denom:
-            pass
-            # incomplete
-			# numberOfWaysToMakeChange(n, denoms, min_denom, results={}, count=0)
-    return count
+		current_denom = 0
+		# reinitialize calculated for each denom calculation
+		calculated = {}
+		while n >= denom:
+			current_denom += denom
+			remaining = n - denom
+			if frozenset([current_denom, remaining]) not in calculated:
+				current_ways += ways[current_denom] * ways[remaining]
+				calculated.add(frozenset([current_denom, remaining]))
+			n = remaining
+	return current_ways
+	
