559bc075f564a405bad333840344692d0ee9f104
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-09-15 01:48:40 -0700

    Added approach & complexity comment

--- Dynamic Programming/minNumberOfCoins.py
+++ Dynamic Programming/minNumberOfCoins.py
@@ -1,27 +1,40 @@
+# Approach: Create a ways array that keeps track of min coins for creating amount 0-n starting with one denom at a time. (Notice that coins to create 0 is 0 (base case)). Whenever a denom can successfully create a number i (current_solution = 1 + ways[i-denom]) (which is != -1), check against existing ways[i] and update result as min(ways[i], current_solution)
+# Complexity: 
+# Time: O(nd) where n is the amount value and d is the number of denominations
+# Space: O(n) space where n is the amount value
+
+# Test following:
+# {"n": 10, "denoms": [3, 4]}  # Expected: 3 {4,3,3}  === Reteuns 3. All Ok!
+
 # Test following:
 # {"n": 10, "denoms": [3, 4]}  # Expected: 3 {4,3,3}
+
+# TODO: Watch video
+# To understand, draw on paper for {"n": 10, "denoms": [3, 4]} & {"n": 6, "denoms": [1, 3, 4]}
 
 def minNumberOfCoinsForChange(n, denoms):
     # create a ways array equal to len(n) and initialize all by -1 except 0. Coins to make 0 is 0 always
 	ways = []
+    # ways[0] = 0. 0 coins can be used to create 0
 	ways.append(0)
 	for i in range(1,n+1):
 		ways.append(-1)
 	
+    # add one denom at a time to the mix
 	for denom in denoms:
 		for num in range(n+1):
-			# set current. current represents a possible answer using current denom
+			# initialize current. current represents a possible answer using current denom
 			current = -1
-			# divisible
+			# if divisible
 			if denom <= num:
-				# current denom can make a solution
+				# if current denom can make a solution
 				if ways[num-denom] != -1:
 					current = 1 + ways[num-denom]
-				# if there was no previous solution, accept this
-				if ways[num] == -1:
-					ways[num] = current
-				# if there was a previous solution, accept minimum
-				else:
-					ways[num] = min(ways[num], current)
-	print(ways)
+					# if there was no previous solution, accept solution with this denom
+					if ways[num] == -1:
+						ways[num] = current
+					# if there was a previous solution, accept minimum
+					else:
+						ways[num] = min(ways[num], current)
     return ways[n]
+
