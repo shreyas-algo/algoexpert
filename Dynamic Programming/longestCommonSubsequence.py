886cf87f8c9e66bdfa3bfccac9b57874f80e4d07
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-09-27 20:45:06 -0700

    3/10 passing

--- Dynamic Programming/longestCommonSubsequence.py
+++ Dynamic Programming/longestCommonSubsequence.py
@@ -1,4 +1,3 @@
-# This solution can work if you initialize both result & resultant_string with 0s and empty strings. Then you just gotta assign. Not do the append thingy which will be updated in upcoming commits
 def longestCommonSubsequence(str1, str2):
     # init
 	result = []
@@ -18,14 +17,17 @@ def longestCommonSubsequence(str1, str2):
 				# initialize contents of the first row
 				result[0].append(0)
 				resultant_string[0].append('')
+				continue
 			
+			# print(row, col)
 			if str1[row-1] == str2[col-1]:
-				result[row][col] = 1 + result[row-1][col-1]
-				resultant_string[row][col] = str1[row-1] + resultant_string[row-1][col-1]
+				result[row].append(1 + result[row-1][col-1])
+				resultant_string[row].append(str1[row-1] + resultant_string[row-1][col-1])
 			else:
 				longer_row, longer_col = (row,col-1) if result[row][col-1] > result[row-1][col] else (row,col-1)
-				result[row][col] = result[longer_row][longer_col]
-				resultant_string[row][col] = resultant_string[longer_row][longer_col]
-				
-	return resultant_string[len_1][len_2]
+				result[row].append(result[longer_row][longer_col])
+				resultant_string[row].append(resultant_string[longer_row][longer_col])
+			
+	print(resultant_string[len_1][len_2])
+	return list(resultant_string[len_1][len_2])
 			
