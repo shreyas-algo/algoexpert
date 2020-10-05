8e42a6ea2711f8eb955ddfce43a7485b1bac2cd3
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-10-04 17:53:38 -0700

    12/12 passing. Added test case specific comment

--- Arrays/largestRange.py
+++ Arrays/largestRange.py
@@ -20,8 +20,12 @@ def largestRange(array):
 		if array[i] - array[i-1] == 1 or array[i] - array[i-1] == 0:
 			curr_end = i
 		else:
-			curr_len = curr_end - curr_start + 1
-			if curr_len > max_len:
+			curr_len = array[curr_end] - array[curr_start] + 1
+			# equality depends on how the interviewer wants it. If they want the last found range or the first first found range if there are two loargest ranges
+            # notice that question mentions that there is only single largest range in the inputs but Test Cse #7 breaks without equality
+            # TODO: check failing cases by removing equality and leave feedback for quesiton if you're right. Equality is important if there can be multiple
+            # [-1, 8] & [10, 19] -- 2 ranges exist with length 10
+            if curr_len >= max_len:
 				# update result related vars
 				max_len = curr_len
 				res_start = curr_start
@@ -29,7 +33,7 @@ def largestRange(array):
 				# update iteration tracking vars
 				curr_start = i
 				curr_end = i
-	curr_len = curr_end - curr_start + 1
-	if curr_len > max_len:
+	curr_len = array[curr_end] - array[curr_start] + 1
+	if curr_len >= max_len:
 		return [array[curr_start], array[curr_end]]
     return [array[res_start], array[res_end]]
