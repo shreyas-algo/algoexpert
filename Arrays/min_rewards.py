e25269dda9cb20a02225685c66e9a98df8ec5ec7
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-10-04 18:31:48 -0700

    Added approach

---
+++ Arrays/min_rewards.py
@@ -0,0 +1,16 @@
+# Root approach: get min one by one and apply counting logic: if received min & previous min are adjacent, increment counter, else use the same counter
+
+# Approach I: Use heap to get min one by one and aply counting logic
+# Approach II: Create an index array from the numbers and sort the 
+
+# Both techniques will be O(nlogn). I think II will need extra O(n) space due to the extra index array  
+# 1. can go with II & discuss improvement of using heap as heap implementation will take time and will be another problem in itself
+# 2. can also abstract heap implementaion and assume it as a library and later code it if you have time
+
+# Learning:
+# !. Weighing two solutions and choosing the best (whichever ensures best results in an interview)
+
+# Implemented II:
+def minRewards(scores):
+    # Write your code here.
+    pass
