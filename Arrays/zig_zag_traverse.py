bedad3009fe1aa3a40345e40f48440bb20a57c32
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-10-09 02:32:52 -0700

    Added plankUp

--- Arrays/zig_zag_traverse.py
+++ Arrays/zig_zag_traverse.py
@@ -14,7 +14,32 @@ def zigzagTraverse(array):
 	# plank functions - append values in current plank and init next plank's row, col
 	while row < row_len and col < col_len:
 		if col == 0 or row == n:
-			row, col = plankUp(array, row, col)
+			row, col = plankUp(array, row, col, row_len, col_len, res)
     	elif row == 0 or col == n:
-			row, col = planDown(array, row, col)
+			row, col = planDown(array, row, col, row_len, col_len, res)
 	return res
+
+def plankUp(array, row, col, row_len, col_len, res):
+	r = row
+	c = col
+	# an until loop 
+	while True:
+		res.append(array[r][c])
+		if r == col and c == row:
+			break
+		r -= 1
+		c += 1
+	# reached final column
+	if c == n:
+		return (r+1,c)
+	# reached top row - r == 0
+	else:
+		return (r,c+1)
+		# Handled in parent while
+		# if r+1 < row_len:
+		# 	return (r+1,c)
+		# Handled in parent while - mostly this case will never happen. This is like r==row_len-1 and c==col_len-1
+		# return (r,c)
+		
+		
+	
