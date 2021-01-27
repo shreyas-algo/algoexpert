8620c789dd7a9787e00637caa36587ca033d4dd9
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2021-01-26 20:18:34 -0800

    Added three number sort solution

---
+++ Sorting/three_number_sort.py
@@ -0,0 +1,6 @@
+# Approach: Used basic python method of sorting an array based on a key 
+# Analysis: O(n log n) where n is size of array
+
+def threeNumberSort(array, order):
+    array.sort(key=lambda x: order.index(x))
+	return array
