89f128c8535f653ed70bd9c39e9a9c197c03b1c7
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2021-01-26 20:45:53 -0800

    Added logic based solution of three number osrt

--- Sorting/three_number_sort.py
+++ Sorting/three_number_sort.py
@@ -1,7 +1,30 @@
-# TODO: Solve using logic
 # Approach: Used basic python method of sorting an array based on a key 
 # Analysis: O(n log n) where n is size of array
 
+# Questions to ask:
+# 1. Will the order be necessarily increasing or decreasing - NO eg desired order can be [0, 1, -1] (arbitrary order)
+
+# Approach I:
 def threeNumberSort(array, order):
     array.sort(key=lambda x: order.index(x))
 	return array
+
+
+# Approach II:
+# Use modified sort algorithm to perform comparison based on order array
+# Analysis: O(n*n)
+def threeNumberSort(array, order):
+    return selectionSort(array, order)
+
+def selectionSort(array, order):
+    arr_len = len(array)
+	for i in range(arr_len-1):
+		# initialize min_idx
+		min_idx = i
+		for j in range(i+1,arr_len):
+            # key change -- in the decision making process, instead of comparing values in array use order in order array
+			if order.index(array[j]) < order.index(array[min_idx]):
+				min_idx = j
+        # swap min from unsorted array with the current index to make the current index a part of the sorted sub array on left
+		array[i], array[min_idx] = array[min_idx], array[i]
+    return array
