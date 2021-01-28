020f97b7be4f0f6e270e1ef3e0874f5502b37bc1
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2021-01-27 21:22:26 -0800

    Corrected naming

--- Greedy/min_waiting_time.py
+++ Greedy/min_waiting_time.py
@@ -1,14 +1,16 @@
-# Approach: sort the array in increasing order (for min waiting time). Keep calculating waiting time as you move along the array. 
-# Note that the waiting time at a pos i will be processed[i-1]
-# where processed[i-1] -> time taken to process i-1 process
-
+# TODO: watch video
+# Approach: sort the array in increasing order (for min waiting time). Keep calculating wait_time as you move along the array and also the wait_time_sum. 
 # Analysis: O(nlogn) & O(1) space
+
 def minimumWaitingTime(queries):
 	queries.sort()
     arr_len = len(queries)
-	processed = 0
 	wait_time = 0
+	wait_time_sum = 0
 	for i in range(1, arr_len):
-	  processed += queries[i-1]
-	  wait_time += processed
-    return wait_time
+        # wait_time at position i
+        wait_time += queries[i-1]
+        # calculate wait_time_sum on the fly
+        wait_time_sum += wait_time
+        # print(a[i], wait_time, wait_time_sum)
+    return wait_time_sum
