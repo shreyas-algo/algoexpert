315fc59fdfd539ffa02ae46b2c107b9c91e9d576
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2021-02-02 22:48:34 -0800

    10/15 passing

---
+++ Strings/run_length_coding.py
@@ -0,0 +1,30 @@
+# 10/15 passing
+def runLengthEncoding(string):
+    count_map = create_count_map(string)
+	print(count_map)
+	encoding = []
+    for key, value in count_map.items():
+		code = find_encoding(key, value)
+		encoding.append(code)
+	print(encoding)
+	return ''.join(encoding)
+
+def create_count_map(string):
+	count_map = {}
+	for char in string:
+		existing_val = count_map.setdefault(char, 0)
+		count_map[char] = existing_val + 1
+	return count_map
+
+def find_encoding(key, value):
+	code = []
+	while True:
+		# if value is single digit, stop
+		if value < 10:
+			break
+		code.append('9'+key)
+		value = value - 9
+	if value != 0:
+		code.append(str(value)+key)
+	print(code)
+	return ''.join(code)
